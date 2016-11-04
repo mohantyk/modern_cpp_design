44b574fb0348ecc75f8a38d161054910c56b8854
Kaniska Mohanty <kaniska@kaniskas-macbook-pro.local>
2016-11-03 19:41:25 -0700

    Initial Commit

---
+++ CplusTutorial/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CplusTutorial
+//
+//  Created by Kaniska Mohanty on 11/3/16.
+//  Copyright © 2016 Kaniska Mohanty. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
