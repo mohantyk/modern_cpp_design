f70569adb0a698a270a665fb13407327c7b0ffb7
Kaniska Mohanty <mohantyk@users.noreply.github.com>
2018-12-14 07:31:54 -0800

    Added typist search.

--- CplusTutorial/Typelists.cpp
+++ CplusTutorial/Typelists.cpp
@@ -8,8 +8,11 @@
 
 #include <vector>
 #include <iostream>
+#include <typeinfo>
 
 #include "Typelists.hpp"
+
+using namespace std;
 
 typedef Typelist<int, NullType> OneTypeOnly;
 typedef Typelist<char, Typelist<signed char, Typelist<unsigned char, NullType> > > AllCharTypes;
@@ -21,7 +24,23 @@ void check_typelist_length(void)
     std::cout << "Length of typelist(should be 4): " << myVector.size() << std::endl;
 }
 
+void check_typelist_indexing(void)
+{
+    typedef TypeAt<SignedIntegrals, 2>::Result IndexedType;
+    std::cout << "Indexed type comparison with int (should be 1): " << (typeid(IndexedType)==typeid(int)) << std::endl;
+    std::cout << "Indexed type comparison with long int (should be 0): " << (typeid(IndexedType)==typeid(long int)) << std::endl;
+    
+}
+
+void check_typelist_search(void)
+{
+    int searchResult(IndexOf<SignedIntegrals, int>::value);
+    cout << "Index of type int in SignedIntegrals (should be 2): " << searchResult << endl;
+}
+
 void typelist_checks(void)
 {
     check_typelist_length();
+    check_typelist_indexing();
+    check_typelist_search();
 }
--- CplusTutorial/Typelists.hpp
+++ CplusTutorial/Typelists.hpp
@@ -10,6 +10,8 @@
 #define Typelists_hpp
 
 #include <stdio.h>
+#include <iostream>
+#include <typeinfo>
 
 template <typename T, typename U>
 struct Typelist
@@ -27,20 +29,65 @@ class NullType {};
 
 // Length of a Typelist
 template <typename TList> struct Length;
+
 template <> struct Length<NullType>
 {
     enum {value = 0};
 };
+
 template <typename T, typename U>
 struct Length<Typelist<T, U> >
 {
     enum { value = 1 + Length<U>::value };
 };
+
 void check_typelist_length(void); // For testing
 
 //Indexed access to a Typelist
-//template <typename TList, unsigned int i>
+template <typename TList, unsigned int i> struct TypeAt;
+
+template <typename Head, typename Tail>
+struct TypeAt<Typelist<Head, Tail>, 0>
+{
+    typedef Head Result;
+};
+
+template <typename Head, typename Tail, unsigned int i>
+struct TypeAt<Typelist<Head, Tail>, i>
+{
+    typedef typename TypeAt<Tail, i-1>::Result Result;
+};
+
+void check_typelist_indexing(void); // For testing
 
 
-void typelist_checks(void);
+// Search a Typelist
+template <typename TList, typename T> struct IndexOf;
+
+template <typename T>
+struct IndexOf<NullType, T>
+{
+    enum { value = -1 };
+};
+
+template <typename Tail, typename T>
+struct IndexOf<Typelist<T, Tail>, T>
+{
+    enum { value = 0 };
+};
+
+template <typename Head, typename Tail, typename T>
+struct IndexOf<Typelist<Head, Tail>, T>
+{
+private:
+    enum { temp = IndexOf<Tail, T>::value } ;
+public:
+    enum { value = temp == -1 ? -1 : 1 + temp} ;
+    
+};
+
+void check_typelist_search(void);
+
+// Wrappers
+void typelist_checks(void); // For testing all typelist functionality
 #endif /* Typelists_hpp */
